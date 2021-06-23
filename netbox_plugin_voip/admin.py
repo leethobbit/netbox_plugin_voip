22eafb77b8533bd02d4bbac54bc6d2ecaa26c134
Dan King <dking8921@gmail.com>
2021-06-23 13:37:54 -0400

    Added admin section to admin.py

--- netbox_plugin_voip/admin.py
+++ netbox_plugin_voip/admin.py
@@ -2,8 +2,13 @@
 from django.contrib import admin
 import .models
 
-
 @admin.register(models.MyModel1)
 class MyModel1Admin(admin.ModelAdmin):
     fields = '__all__'
 """
+from django.contrib import admin
+from .models import DIDNumbers
+
+@admin.register(DIDNumbers)
+class DIDVoipAdmin(admin.ModelAdmin):
+    list_display = ("did", "description","partition")
